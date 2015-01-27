8783c60fd6e42d9ee6807c39d6b64889bf9aa864
Toni Leigh <toni.leigh@sage.com>
2015-01-27 10:15:52 +0000

    adds some failed expectations for input processing

--- spec/lib/input_spec.rb
+++ spec/lib/input_spec.rb
@@ -5,9 +5,19 @@ describe Input do
 
 	it "should read the cipher file" do
 
+		expect( IO.read(subject.file_source).length ).to be > 0
+
+		expect( subject.file_data ).to be_kind_of (String)
+
+		expect( subject.file_data.length ).to be > 0
+
 	end
 
 	it "should create a new CryptBoard object" do
+
+		expect( subject.file_target_object ).to be_kind_of (CryptBoard)
+
+		expect( subject.file_target_object ).to respond_to (:convert_data)
 
 	end
 
