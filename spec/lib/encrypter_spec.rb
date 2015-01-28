3be994c655ac811471f1d092a9471dd5a680829e
Toni Leigh <toni.leigh@sage.com>
2015-01-28 14:11:46 +0000

    whitespace

--- spec/lib/encrypter_spec.rb
+++ spec/lib/encrypter_spec.rb
@@ -11,7 +11,6 @@ describe Encrypter do
 	end
 
 	context "Boxes - functionality identical whether encrypting or decrypting" do
-
 		it "should replace any two characters on the corners of squares where both dimensions are at least 2 with the characters from the opposite corners - character rows must be kept in the same order" do
 			expect( encrypter.encrypt_pair('R','X') ).to be == ['H','V']
 			expect( encrypter.encrypt_pair('V','Y') ).to be == ['X','L']
@@ -20,79 +19,59 @@ describe Encrypter do
 			expect( encrypter.encrypt_pair('B','T') ).to be == ['M','N']
 			expect( encrypter.encrypt_pair('A','X') ).to be == ['Y','W']
 		end
-
 	end
 
-	context "Encrypting" do		
-
+	context "Encrypting" do
 		it "should strip the spaces and should append a 'Z' if it's got an odd number of chars" do
 			encrypter.encrypt_message
 			encrypter.decrypt_message
 			expect( encrypter.message ).to be == 'THISISATESTMESSAGETHISTESTMESSAGEMUSTBEENCRYPTEDCAREFULLYZ'
 		end
-
 		it "should replace any pair of equal characters in the message with the string <char1><q>" do
 			encrypter.encrypt_message
 			encrypter.decrypt_message
 			expect( encrypter.decrypted ).to be == 'THISISATESTMESSAGETHISTESTMESQAGEMUSTBEQNCRYPTEDCAREFULQYZ'
 		end
-
 		context "Rows and columns" do
-
 			it "should replace a pair of characters in the same row with the characters one cell to the right" do
 				expect( encrypter.encrypt_pair('P','L') ).to be == ['L','A']
 			end
-
 			it "should replace the character in cell five with the character in cell one of the same row" do
 				expect( encrypter.encrypt_pair('Y','F') ).to be == ['F','P']
 			end
-
 			it "should replace a pair of characters in the same column with the characters one cell down" do
 				expect( encrypter.encrypt_pair('D','O') ).to be == ['O','V']
 			end
-
 			it "should replace the character in cell five with the character in cell one of the same column" do
 				expect( encrypter.encrypt_pair('S','X') ).to be == ['X','Y']
 			end
-
 		end
-
 		it "should create an encrypted message" do
 			encrypter.encrypt_message
 			expect( encrypter.encrypted ).to be == 'SEHNHNFQHTZTHTQYMCSEHNZMTNTMTSCQMTXNNMCTQIHLFNRMGCCIPZAOFX'
 		end
-
 	end
 
 	context "Decrypting" do
-
 		context "Rows and columns" do
-
 			it "should replace a pair of characters in the same row with the characters one cell to the left" do
 				expect( encrypter.encrypt_pair('L','A',false) ).to be == ['P','L']
 			end
-
 			it "should replace the character in cell one with the character in cell five of the same row" do
 				expect( encrypter.encrypt_pair('F','P',false) ).to be == ['Y','F']
 			end
-
 			it "should replace a pair of characters in the same column with the characters one cell down" do
 				expect( encrypter.encrypt_pair('O','V',false) ).to be == ['D','O']
 			end
-
 			it "should replace the character in cell one with the character in cell five of the same column" do
 				expect( encrypter.encrypt_pair('X','Y',false) ).to be == ['S','X']
 			end
-
 		end
-
 		it "should store the unencrypted message" do
 			encrypter.encrypt_message
 			encrypter.decrypt_message
 			expect( encrypter.decrypted ).to be == 'THISISATESTMESSAGETHISTESTMESQAGEMUSTBEQNCRYPTEDCAREFULQYZ'
-
 		end
-
 	end
-
+	
 end
